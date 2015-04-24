e029586bae53415fc2114c8e5514548bfc9b1b04
Josep M Sobrepere <josep@rangle.io>
2015-04-23 23:34:09 -0400

    Adding streams-generator.js

---
+++ js/streams-generator.js
@@ -0,0 +1,62 @@
+var getdimensions = require('./dimensions.js');
+var Bacon = require('baconjs');
+
+function getDimensionsStream(load$, resize$){
+  return load$.merge(resize$)
+    .map(function (){
+      return {
+        width: window.innerWidth * 2,
+        height: window.innerHeight * 2
+      };
+    })
+    .map(getdimensions);
+}
+
+function getSpaceStream(keyUp$){
+  return keyUp$
+    .map(function(e){
+      return e.which === 32;
+    })
+    .filter(function(x){return x;});
+}
+
+function getDirectionStream(keyUp$, space$){
+  var arrows$ = keyUp$.map(function(e){
+    switch(e.which){
+      case 37:
+        return 'LEFT';
+      case 38:
+        return 'UP';
+      case 39:
+        return 'RIGHT';
+      case 40:
+        return 'DOWN';
+      default:
+        return null;
+    }
+  })
+  .filter(R.compose(R.not, R.isNil));
+
+  var direction$ = Bacon.when(
+    [space$], function(){return null;},
+    [arrows$], function(a){return a;}
+  )
+  .skipDuplicates();
+
+  return direction$.diff(null, function(prev, last){
+    var lastTwo = [prev, last].sort().join('-');
+    return (['LEFT-RIGHT', 'DOWN-UP'].indexOf(lastTwo)>-1) ?
+      'SKIP':
+      last;
+  })
+  .filter(function(d){
+    return d!=='SKIP';
+  })
+  .changes();
+}
+
+module.exports = {
+  getDimensionsStream: getDimensionsStream,
+  getSpaceStream: getSpaceStream,
+  getDirectionStream: getDirectionStream
+};
--- main.js
+++ main.js
@@ -1,66 +1,25 @@
 var R = require('ramda');
 var Bacon = require('baconjs');
 var Immutable = require('immutable');
-var getdimensions = require('./js/dimensions.js');
-var renderer = require('./js/render.js');
+
+var streams = require('./js/streams-generator.js');
 var ticker = require('./js/ticker.js');
 var snakeHelper = require('./js/snake.js');
 var getFood = require('./js/food.js');
+var renderer = require('./js/render.js');
 
 //EventStream variables end with $, i.e: ticks$
 //Propertie varibles end with $$, i.e: game$$
 
-//dimensions stream
-var load$ = $(window).asEventStream('load');
-var resize$ = $(window).asEventStream('resize');
-var dimensions$ =
-  load$.merge(resize$)
-      .map(function (){
-        return {
-          width: window.innerWidth * 2,
-          height: window.innerHeight * 2
-        };
-      })
-      .map(getdimensions);
-
-$(document).on('ready', function(){
+function main(){
+  var dimensions$ = streams.getDimensionsStream(
+    $(window).asEventStream('load'),
+    $(window).asEventStream('resize')
+  );
   var ticks$ = ticker(100);
   var keyUp$ = $(document).asEventStream('keyup');
-  var arrows$ = keyUp$.map(function(e){
-    switch(e.which){
-      case 37:
-        return 'LEFT';
-      case 38:
-        return 'UP';
-      case 39:
-        return 'RIGHT';
-      case 40:
-        return 'DOWN';
-      default:
-        return null;
-    }
-  }).filter(R.compose(R.not, R.isNil));
-
-  var space$ = keyUp$.map(function(e){
-    return e.which === 32;
-  }).filter(function(x){return x;});
-
-  var direction$ = Bacon.when(
-    [space$], function(){return null;},
-    [arrows$], function(a){return a;}
-  )
-  .skipDuplicates();
-
-  direction$ = direction$.diff(null, function(prev, last){
-    var lastTwo = [prev, last].sort().join('-');
-    return (['LEFT-RIGHT', 'DOWN-UP'].indexOf(lastTwo)>-1) ?
-      'SKIP':
-      last;
-  })
-  .filter(function(d){
-    return d!=='SKIP';
-  })
-  .changes();
+  var space$ = streams.getSpaceStream(keyUp$);
+  var direction$ = streams.getDirectionStream(keyUp$, space$);
 
   var direction$$ = direction$.toProperty();
   var dimensions$$ = dimensions$.toProperty();
@@ -85,7 +44,7 @@ $(document).on('ready', function(){
     var end = snakeHelper.isThereCoalition(newSnake, dimensions.rows,
                                            dimensions.cols);
     if(!end && newPosition.x == old.food.x && newPosition.y == old.food.y){
-      newGrowthLeft += 3;
+      newGrowthLeft += 4;
       newFood = getFood(newSnake.toArray(), dimensions.rows, dimensions.cols);
     }
     return {
@@ -118,4 +77,6 @@ $(document).on('ready', function(){
   Bacon.onValues(dimensions$, game$$, function(dimensions, game){
     render(dimensions, game.snake, game.food);
   });
-});
+}
+
+$(main);
