3c1e3f9684fa6487e606cfe86ef9f6bae3861375
Josep M Sobrepere <josep@rangle.io>
2015-04-30 01:27:51 -0400

    rearranging the main function

--- main.js
+++ main.js
@@ -18,44 +18,38 @@ var getAvailablePosition =
  * ************************************************/
 
 function main(){
-  var keyUp$ = $(window).asEventStream('keyup');
-  var space$ = streams.getKeyStream(keyUp$, constants.KEYBOARD_KEYS.SPACE);
-  var dimensions$ = streams.getDimensionsStream(
-    $(window).asEventStream('load'),
-    $(window).asEventStream('resize')
-  );
-
   var gameStart$ = new Bacon.Bus();
   var gameEnd$ = new Bacon.Bus();
   var gameEvents$ = gameStart$.map('START').merge(gameEnd$.map('END'));
   var gameActive$$ = gameEvents$.scan(false, R.flip(R.eq('START')));
-  var paused$$ = space$.filter(gameActive$$)
-                       .scan(false, R.not);
 
-  var direction$ = streams.getDirectionStream(
-                      keyUp$.filter(gameActive$$)
-                            .filter(paused$$.not()),
-                      gameEnd$);
+  var keyUp$ = $(window).asEventStream('keyup');
+  var space$ = streams.getKeyStream(keyUp$, constants.KEYBOARD_KEYS.SPACE);
+  var paused$$ = space$.filter(gameActive$$).scan(false, R.not);
+
+  var direction$ = streams.getDirectionStream(keyUp$.filter(gameActive$$)
+                                                    .filter(paused$$.not()),
+                                              gameEnd$);
+  var direction$$ = direction$.toProperty();
   var ticks$ = streams.getTicksStream(config.TICK_FREQUENCY)
                       .skipUntil(direction$)
                       .filter(gameActive$$)
                       .filter(paused$$.not());
-  var direction$$ = direction$.toProperty();
   var head$ = Bacon.update(
     null,
     [gameStart$], R.nAry(0, getAvailablePosition),
     [direction$, ticks$], helpers.getNextHeadPosition,
     [direction$$, ticks$], helpers.getNextHeadPosition
   )
-  .skipDuplicates()
   .changes();
-
   var snakeAndFood = getSnakeAndFood(head$, gameStart$);
   var snake$$ = snakeAndFood.snake$$;
   var food$$ = snakeAndFood.food$$;
 
-  var startNewGame$ = space$.filter(gameActive$$.not());
-
+  var dimensions$ = streams.getDimensionsStream(
+    $(window).asEventStream('load'),
+    $(window).asEventStream('resize')
+  );
   var ctx = $('canvas')[0].getContext('2d');
   var render = renderer(ctx);
   Bacon
@@ -65,8 +59,9 @@ function main(){
      gameEnd$.push(Date.now());
     }
   });
+
   gameStart$.push(Date.now());
-  gameStart$.plug(startNewGame$.map(Date.now()));
+  gameStart$.plug(space$.filter(gameActive$$.not()).map(Date.now()));
 }
 
 $(main);
